477d2ea2705533b55118f4380776d0756511590a
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2023-04-23 20:34:24 -0300

    Removed unsued import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Link from './Link'
-import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import headerNavLinks from '@/data/headerNavLinks'
 
