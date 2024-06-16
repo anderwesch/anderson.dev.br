4b669a6a92986c3e6533af1d8a7c38ca1689fd01
Anderson Weschnhoski <andersonweschnhoski@gmail.com>
2024-06-15 22:01:26 -0300

    Update metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Anderson Blog',
+  title: 'anderson.dev Blog',
   author: 'Anderson Weschnhoski',
   headerTitle: 'anderson.dev',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Software engineer and architect with expertise in full-stack development, scalable system architecture, and cloud computing',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -12,14 +12,14 @@ const siteMetadata = {
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
+  github: 'https://github.com/anderwesch',
+  x: 'https://twitter.com/anderwesch',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  //facebook: 'https://facebook.com',
+  //youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com/in/anderson-weschnhoski/',
+  //threads: 'https://www.threads.net',
+  //instagram: 'https://www.instagram.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
@@ -46,7 +46,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    // provider: 'buttondown',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
